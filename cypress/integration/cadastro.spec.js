aec67a56c14b7787d0c191cb05ccab8ca3cf22b4
Jaime <jaimegenerali@gmail.com>
2022-05-18 18:12:57 -0300

    oranizcçao melhor

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,18 +1,32 @@
 import cadastro from '../support/pages/CadastroPage'
 import login from '../support/pages/LoginPage'
 describe('Novo usuário e Login com sucesso', function () {
-    it('Cadastro de novo usuário com sucesso', function () {
+    it.only('Cadastro de novo usuário com sucesso', function () {
         cadastro.go()
         cadastro.signinClick()
         cadastro.email_address()
         cadastro.btn_createAnaccount()
-        cadastro.personal_information()
-        cadastro.your_address()
-        cadastro.btn_register()
+        cadastro.gender()
+        cadastro.name_lastname('James', 'francisco')
+        cadastro.password()
+        cadastro.dateOfBirth()
+        cadastro.checkbox()
+        cadastro.company()
+        cadastro.address1()
+        cadastro.address2()
+        cadastro.city()
+        cadastro.state()
+        cadastro.postal_code()
+        cadastro.country()
+        cadastro.phone()
+        cadastro.mobile_phone()
+        cadastro.address_alias()
+        //cadastro.btn_register()
     });
 
-    it.only('Cadastro de usuário sem preencher dados obrigatórios', function () {
+    it('cadastro sem preencher first name', function () {
         cadastro.go()
         cadastro.signinClick()
+
     });
 });
--- cypress/support/pages/CadastroPage.js
+++ cypress/support/pages/CadastroPage.js
@@ -5,43 +5,87 @@ class CadastroPage {
         cy.visit('http://automationpractice.com/index.php');
         cy.title().should('include', 'My Store')
     }
+    
     signinClick() {
         cy.get('.login').click();
         cy.get('#create-account_form > .form_content > p').should('have.text', 'Please enter your email address to create an account.');
     }
+    
     email_address() {
-        cy.get('#email_create').type('akak1@gmail.com')
+        cy.get('#email_create').type('akak121@gmail.com')
     }
+    
     btn_createAnaccount() {
         cy.get('#SubmitCreate > span').click();
         cy.wait(6000);
     }
-    personal_information() {
+    
+    gender() {
         cy.get('#id_gender1').click();
         cy.get(':nth-child(1) > .page-subheading').should('have.text', 'Your personal information')
-        cy.get('#customer_firstname').type('Krebinho');
-        cy.get('#customer_lastname').type('Krebiano');
+    }
+    
+    name_lastname(name, last_name) {
+        cy.get('#customer_firstname').type(name);
+        cy.get('#customer_lastname').type(last_name);
+    };
+    
+    password() {
         cy.get('#passwd').type('12345');
+    }
+    
+    dateOfBirth() {
         cy.get('[id="days"]').select('1');
         cy.get('[id="months"]').select('5');
         cy.get('[id="years"]').select('1990');
+        
+    }
+    checkbox() {
         cy.get('#newsletter').check();
         cy.get('#optin').check();
-    }
-    your_address() {
         cy.get(':nth-child(2) > .page-subheading').should('have.text', 'Your address');
+    }
+    company() {
         cy.get('#company').type('CEOCypress');
-        cy.get('#address1').type('Rua dos afogados, n2, Bairro Francisco');
-        cy.get('#address2').type('rua dos pudins, n4, bairro Capim');
+    }
+                
+    address1() {
+        cy.get('#address1').type('Rua dos frangos');    
+    }
+
+    address2() {
+        cy.get('#address2').type('n330, apartamento 2');
+    }
+
+    city(){
         cy.get('#city').type('Lagarto');
+    }
+    
+    state(){
         cy.get('[id="id_state"]').select(2);
-        cy.get('#postcode').type('20202020')
+    } 
+    postal_code(){
+        cy.get('#postcode').type('20202020');
+    } 
+    country() {
+        cy.get('[id="id_country"]').select('21')
+    
+    }
+    phone() {
         cy.get('#phone').type('27736599');
+    }
+
+    mobile_phone() {
         cy.get('#phone_mobile').type('21999991234');
+    }
+    
+    address_alias() {
         cy.get('input[id="alias"]').clear().type('TheOne');
     }
+
     btn_register() {
         cy.get('#submitAccount > span').click();
     }
 }
+
 export default new CadastroPage()
